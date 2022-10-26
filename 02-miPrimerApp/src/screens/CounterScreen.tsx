f0ec62e6d73049ce75dee3d8108186408b5c4008
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-10-26 15:55:36 -0300

    Seccion 04: 55. StyleSheet

--- 02-miPrimerApp/src/screens/CounterScreen.tsx
+++ 02-miPrimerApp/src/screens/CounterScreen.tsx
@@ -1,43 +1,34 @@
 import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  Button,
-  TouchableOpacity,
-  TouchableOpacityComponent,
-} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 export function CounterScreen() {
   const [contador, setContador] = useState(10);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        // El 1 abarca todo el espacio posible del padre
-        justifyContent: 'center',
-      }}>
-      <Text
-        style={{
-          textAlign: 'center',
-          fontSize: 40,
-          // Por defecto todo es posicion relativa
-          top: -15,
-        }}>
-        Contador: {contador}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {contador}</Text>
 
-      <TouchableOpacity
-        onPress={() => setContador(contador + 1)}
-      >
-        <View
-          style={{
-            backgroundColor: 'red',
-            borderRadius: 100,
-          }}>
+      <TouchableOpacity onPress={() => setContador(contador + 1)}>
+        <View style={styles.buttonIncrement}>
           <Text>+1</Text>
         </View>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    top: -15,
+  },
+  buttonIncrement: {
+    backgroundColor: 'red',
+    borderRadius: 100,
+  },
+});

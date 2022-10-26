ea284edd5c028afd253abb492836507a57490af7
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-10-26 16:23:53 -0300

    Seccion 04: 56. Boton Personalizado flotante

--- 02-miPrimerApp/src/screens/CounterScreen.tsx
+++ 02-miPrimerApp/src/screens/CounterScreen.tsx
@@ -8,9 +8,19 @@ export function CounterScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Contador: {contador}</Text>
 
-      <TouchableOpacity onPress={() => setContador(contador + 1)}>
-        <View style={styles.buttonIncrement}>
-          <Text>+1</Text>
+      <TouchableOpacity
+        style={styles.fabLocationBR}
+        onPress={() => setContador(contador + 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.fabLocationBL}
+        onPress={() => setContador(contador - 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -23,12 +33,31 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    textAlign: 'center',
     fontSize: 40,
+    textAlign: 'center',
     top: -15,
   },
-  buttonIncrement: {
-    backgroundColor: 'red',
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 50,
+    right: 15,
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 50,
+    left: 15,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
     borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
   },
 });

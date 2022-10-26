bd98034f42d070a5a0ed45a99f10330c80319964
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-10-26 16:59:44 -0300

    Seccion 05: 58. Enviar funciones y propiedades opcionales

--- 02-miPrimerApp/src/screens/CounterScreen.tsx
+++ 02-miPrimerApp/src/screens/CounterScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import { Fab } from '../components/Fab';
 
 export function CounterScreen() {
@@ -9,23 +9,16 @@ export function CounterScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Contador: {contador}</Text>
 
-      {/* <TouchableOpacity
-        style={styles.fabLocationBR}
-        onPress={() => setContador(contador + 1)}>
-        <View style={styles.fab}>
-          <Text style={styles.fabText}>+1</Text>
-        </View>
-      </TouchableOpacity>
+      <Fab
+        title= "+1"
+        onPress={() => setContador(contador + 1)}
+        position= "br"
+      />
 
-      <TouchableOpacity
-        style={styles.fabLocationBL}
-        onPress={() => setContador(contador - 1)}>
-        <View style={styles.fab}>
-          <Text style={styles.fabText}>-1</Text>
-        </View>
-      </TouchableOpacity> */}
       <Fab
         title= "-1"
+        onPress={() => setContador(contador - 1)}
+        position= "bl"
       />
     </View>
   );
@@ -41,27 +34,4 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     top: -15,
   },
-  // fabLocationBR: {
-  //   position: 'absolute',
-  //   bottom: 50,
-  //   right: 15,
-  // },
-  // fabLocationBL: {
-  //   position: 'absolute',
-  //   bottom: 50,
-  //   left: 15,
-  // },
-  // fab: {
-  //   backgroundColor: '#5856D6',
-  //   width: 60,
-  //   height: 60,
-  //   borderRadius: 100,
-  //   justifyContent: 'center',
-  // },
-  // fabText: {
-  //   color: 'white',
-  //   fontSize: 25,
-  //   fontWeight: 'bold',
-  //   alignSelf: 'center',
-  // },
 });

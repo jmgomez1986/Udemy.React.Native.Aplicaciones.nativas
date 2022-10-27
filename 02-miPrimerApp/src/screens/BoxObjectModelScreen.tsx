389a091b7b4a4a06203e99abc760cca981b3f328
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-10-27 14:30:05 -0300

    Sesion 05: 67. Padding, Margin, Border, Width y Height

--- 02-miPrimerApp/src/screens/BoxObjectModelScreen.tsx
+++ 02-miPrimerApp/src/screens/BoxObjectModelScreen.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export const BoxObjectModelScreen = () => {
   return (
-    <View>
+    <View style={ styles.container }>
 
-      <Text>Box Object Model</Text>
+      <Text style={ styles.title }>Box Object Model</Text>
 
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    // padding: 20,
+    paddingHorizontal: 100,
+    paddingVertical: 20,
+    fontSize: 20,
+    marginHorizontal: 20,
+    // width: 250,
+    borderWidth: 10,
+    // backgroundColor: 'red',
+  },
+});

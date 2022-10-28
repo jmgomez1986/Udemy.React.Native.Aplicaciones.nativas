429cc5cb9936a8cac6852af57d61598e7dfb0b71
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-10-28 10:41:43 -0300

    Seccion 05: 71. Pocision absoluta

--- 02-miPrimerApp/src/screens/PositionScreen.tsx
+++ 02-miPrimerApp/src/screens/PositionScreen.tsx
@@ -4,6 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 export const PositionScreen = () => {
   return (
     <View style={ styles.container }>
+      <View style={ styles.cajaVerde }></View>
       <View style={ styles.cajaMorada }></View>
       <View style={ styles.cajaNaranja }></View>
     </View>
@@ -13,8 +14,10 @@ export const PositionScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // width: 400,
+    // height: 400,
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#28C4D9',
   },
   cajaMorada: {
@@ -23,8 +26,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#5856D6',
     borderWidth: 10,
     borderColor: 'white',
-    // top: 100,
-    // left: 100,
+    position: 'absolute',
+    top: 0,
+    right: 0,
   },
   cajaNaranja: {
     width: 100,
@@ -32,6 +36,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0A23B',
     borderWidth: 10,
     borderColor: 'white',
-    top: -50.
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  cajaVerde: {
+    // width: 100,
+    // height: 100,
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: 'white',
+    // position: 'absolute',
+    // top: 0,
+    // bottom: 0,
+    // left: 0,
+    // right: 0,
+    ...StyleSheet.absoluteFillObject,
   },
 });

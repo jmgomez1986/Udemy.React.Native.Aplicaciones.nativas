400e89fc40e1958f3e108eb5139373a89278f4af
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-10-28 14:14:36 -0300

    Seccion 05: Flex Wrap

--- 02-miPrimerApp/src/screens/FlexScreen.tsx
+++ 02-miPrimerApp/src/screens/FlexScreen.tsx
@@ -7,6 +7,42 @@ export const FlexScreen = () => {
       <Text style={ styles.caja1 }>Caja 1</Text>
       <Text style={ styles.caja2 }>Caja 2</Text>
       <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
+      <Text style={ styles.caja1 }>Caja 1</Text>
+      <Text style={ styles.caja2 }>Caja 2</Text>
+      <Text style={ styles.caja3 }>Caja 3</Text>
     </View>
   );
 };
@@ -15,25 +51,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28C4D9',
-    flexDirection: 'row',
     alignItems: 'flex-start',
+    flexWrap: 'wrap',
   },
   caja1: {
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
-    // alignSelf: 'center',
   },
   caja2: {
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
-    alignSelf: 'flex-start',
   },
   caja3: {
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
-    alignSelf: 'flex-end',
   },
 });

ce8099f451e475c5e7e6df6af07dc919e35a0441
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-10-31 16:34:03 -0300

    Seccion 06: 87. Diseño inicial y pantalla de Calculadora

--- 03-calculadora/.eslintrc.js
+++ 03-calculadora/.eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 0,
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/no-unused-vars': 1,
         'no-shadow': 'off',
         'no-undef': 'off',
+        'react-native/no-inline-styles': 'off',
       },
     },
   ],
--- 03-calculadora/App.tsx
+++ 03-calculadora/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {CalculadoraScreen} from './src/screens/CalculadoraScreen';
+import {styles} from './src/theme/appTheme';
 
 const App = () => {
   return (
-    <View>
-      <Text>Calculadora</Text>
-    </View>
+    <SafeAreaView style={styles.background}>
+      <StatusBar
+        backgroundColor="black"
+        barStyle="light-content" />
+      <CalculadoraScreen />
+    </SafeAreaView>
   );
 };
 

9e447d4f552f03f6e942e53647d9eacba195f524
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-10-31 16:54:08 -0300

    Seccion 06: 88. Textos y mi primer Boton

--- 03-calculadora/src/screens/CalculadoraScreen.tsx
+++ 03-calculadora/src/screens/CalculadoraScreen.tsx
@@ -4,8 +4,24 @@ import { styles } from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
   return (
-    <View>
-      <Text style={styles.text}>CalculadoraScreen</Text>
+    <View style={styles.calculadoraContainer}>
+      <Text style={styles.resultadoChico}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
+
+
+      <View>
+
+        {/* Boton */}
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>
+            1
+          </Text>
+        </View>
+
+
+      </View>
+
+
     </View>
   );
 };
--- 03-calculadora/src/theme/appTheme.tsx
+++ 03-calculadora/src/theme/appTheme.tsx
@@ -1,12 +1,37 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+  calculadoraContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'flex-end',
+  },
   background: {
     flex: 1,
     backgroundColor: 'black',
   },
-  text: {
+  resultado: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  resultadoChico: {
+    color: 'rgba(255, 255, 255, .5)',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+  button: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#333333',
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'white',
+    fontWeight: '400',
   },
 });

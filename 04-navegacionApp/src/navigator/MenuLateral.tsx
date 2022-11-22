05d5ab2393dfc6acdf308c25622c6f5fba6e6173
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-11-22 15:59:54 -0300

    Seccion 07: 114. Navegar desde el MenuLateral personalizado

--- 04-navegacionApp/src/navigator/MenuLateral.tsx
+++ 04-navegacionApp/src/navigator/MenuLateral.tsx
@@ -1,9 +1,13 @@
-import {createDrawerNavigator, DrawerContentComponentProps, DrawerContentScrollView} from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+} from '@react-navigation/drawer';
 import React from 'react';
 import {SettingsScreen} from '../screens/SettingsScreen';
 import {StackNavigator} from './StackNavigator';
-import {Text, View, Image, useWindowDimensions} from 'react-native';
-import { styles } from '../theme/appTheme';
+import {Text, View, Image, useWindowDimensions, TouchableOpacity} from 'react-native';
+import {styles} from '../theme/appTheme';
 
 const Drawer = createDrawerNavigator();
 
@@ -16,32 +20,42 @@ export const MenuLateral = () => {
         drawerType: width >= 768 ? 'permanent' : 'front',
         headerShown: false,
       }}
-      drawerContent={(props) => <MenuInterno {...props} />}>
-      <Drawer.Screen
-        name="StackNavigator"
-        component={StackNavigator}
-      />
-      <Drawer.Screen
-        name="SettingsScreen"
-        component={SettingsScreen}
-      />
+      drawerContent={props => <MenuInterno {...props} />}>
+      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
     </Drawer.Navigator>
   );
 };
 
-const MenuInterno = (props: DrawerContentComponentProps) => {
+const MenuInterno = ({navigation}: DrawerContentComponentProps) => {
   return (
     <DrawerContentScrollView>
-
+      {/* Parte del Avatar */}
       <View style={styles.avatarContainer}>
         <Image
           style={styles.avatar}
           source={{
-            uri: "https://www.milton.edu/wp-content/uploads/2019/11/avatar-placeholder.jpg"
+            uri: 'https://www.milton.edu/wp-content/uploads/2019/11/avatar-placeholder.jpg',
           }}
         />
       </View>
 
+      {/* Opciones de Menú */}
+      <View style={styles.menuContainer}>
+        <TouchableOpacity
+          style={styles.menuButton}
+          onPress={() => navigation.navigate('StackNavigator')}
+          >
+          <Text style={styles.menuItem}>Navegación</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.menuButton}
+          onPress={() => navigation.navigate('SettingsScreen')}
+          >
+          <Text style={styles.menuItem}>Ajustes</Text>
+        </TouchableOpacity>
+      </View>
     </DrawerContentScrollView>
-  )
+  );
 };
--- 04-navegacionApp/src/theme/appTheme.tsx
+++ 04-navegacionApp/src/theme/appTheme.tsx
@@ -31,4 +31,14 @@ export const styles = StyleSheet.create({
     height: 150,
     borderRadius: 100,
   },
+  menuContainer: {
+    marginVertical: 30,
+    marginHorizontal: 50,
+  },
+  menuItem: {
+    fontSize: 20,
+  },
+  menuButton: {
+    marginVertical: 10,
+  },
 });

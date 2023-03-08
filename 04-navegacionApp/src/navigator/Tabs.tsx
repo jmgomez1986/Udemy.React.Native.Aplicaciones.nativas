2f0721e05d4d6de0a314c7198e0c3755ef72c719
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-03-08 12:42:24 -0300

    Seccion08: 123. Personalizando el BottomTabNavigator

--- 04-navegacionApp/src/navigator/Tabs.tsx
+++ 04-navegacionApp/src/navigator/Tabs.tsx
@@ -3,15 +3,29 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Tab1Screen} from '../screens/Tab1Screen';
 import {Tab2Screen} from '../screens/Tab2Screen';
 import {Tab3Screen} from '../screens/Tab3Screen';
+import { StackNavigator } from './StackNavigator';
+import { colours } from '../theme/appTheme';
 
 const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
-      <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
-      <Tab.Screen name="Tab3Screen" component={Tab3Screen} />
+    <Tab.Navigator
+    sceneContainerStyle= {{
+      backgroundColor: 'white',
+    }}
+      screenOptions={{
+        tabBarActiveTintColor: colours.primary,
+        tabBarStyle: {
+          borderTopColor: colours.primary,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+      }}
+    >
+      <Tab.Screen name="Tab1Screen" options={{title: 'Tab1'}} component={Tab1Screen} />
+      <Tab.Screen name="Tab2Screen" options={{title: 'Tab2'}} component={Tab2Screen} />
+      <Tab.Screen name="StackNavigator" options={{title: 'Stack'}} component={StackNavigator} />
     </Tab.Navigator>
   );
-}
+};

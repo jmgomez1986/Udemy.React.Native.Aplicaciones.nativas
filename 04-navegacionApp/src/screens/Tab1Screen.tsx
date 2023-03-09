02b2cd2a2dc9098498b87915e591386b7adfdd66
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-03-09 11:38:16 -0300

    Seccion 08: 128. Iconos en nuestra aplicación - Instalaciones

--- 04-navegacionApp/src/screens/Tab1Screen.tsx
+++ 04-navegacionApp/src/screens/Tab1Screen.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Text, View } from 'react-native';
+import { styles, colours } from '../theme/appTheme';
+
 
 export const Tab1Screen = () => {
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Icons</Text>
+      <Text>
+        <Icon name="airplane-outline" size={80} color={colours.primary} />
+      </Text>
     </View>
   );
 };

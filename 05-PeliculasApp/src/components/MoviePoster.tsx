f04ced0623c4080d5a20ba47ec5ba6ec0a2b9ffe
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-03-14 17:01:25 -0300

    Seccion 10: 156. FlatList de Películas

--- 05-PeliculasApp/src/components/MoviePoster.tsx
+++ 05-PeliculasApp/src/components/MoviePoster.tsx
@@ -3,16 +3,19 @@ import { Image, View, StyleSheet } from 'react-native';
 import { Movie } from '../interfaces/movieInterface';
 
 interface Props {
-  movie: Movie
+  movie: Movie,
+  width?: number,
+  height?: number,
 }
 
-export const MoviePoster = ({ movie }: Props) => {
+export const MoviePoster = ({ movie, width = 300, height = 420 }: Props) => {
   const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   return (
     <View style={{
-        width: 300,
-        height: 420,
+        width,
+        height,
+        marginHorizontal: 8,
       }}
     >
       <View style={ styles.imageContainer}>
--- 05-PeliculasApp/src/screens/HomeScreen.tsx
+++ 05-PeliculasApp/src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, View, Dimensions } from 'react-native';
+import { ActivityIndicator, View, Dimensions, Text, FlatList, ScrollView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
 import { MoviePoster } from '../components/MoviePoster';
@@ -24,15 +24,38 @@ export const HomeScreen = () => {
   }
 
   return (
-    <View style={{marginTop: marginTop + 20}}>
-      <View style={{height: 420}}>
-        <Carousel
-          data={moviesInCinema}
-          renderItem={({item}) => <MoviePoster movie={item} />}
-          sliderWidth={windowWidth}
-          itemWidth={300}
-        />
+    <ScrollView>
+
+      <View style={{marginTop: marginTop + 20}}>
+
+        <View style={{height: 420, marginBottom: 20}}>
+          <Carousel
+            data={moviesInCinema}
+            renderItem={({item}) => <MoviePoster movie={item} />}
+            sliderWidth={windowWidth}
+            itemWidth={300}
+          />
+        </View>
+
+        <View style={{backgroundColor: 'red', height: 270}}>
+
+          <Text style={{fontSize: 30, fontWeight: 'bold', marginBottom: 10}}>
+            En Cartelera
+          </Text>
+
+          <FlatList
+            data={moviesInCinema}
+            renderItem={({item}) =>
+              <MoviePoster movie={item} width={140} height={200} />}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+          />
+
+        </View>
+
       </View>
-    </View>
+
+    </ScrollView>
   );
 };

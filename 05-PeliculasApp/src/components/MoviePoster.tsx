8e048ef534f16c486b456823f3f072e9ab9f10ee
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-03-15 14:25:49 -0300

    Seccion 10: 160. Navegar a la pantalla de detalles

--- 05-PeliculasApp/src/components/MoviePoster.tsx
+++ 05-PeliculasApp/src/components/MoviePoster.tsx
@@ -1,18 +1,26 @@
+import { useNavigation } from '@react-navigation/core';
+import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { Image, View, StyleSheet } from 'react-native';
+import { Image, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Movie } from '../interfaces/movieInterface';
-
+import { RootStackParams } from '../navigation/Navigation';
 interface Props {
   movie: Movie,
   width?: number,
   height?: number,
 }
 
+type DetailscreenNavigationProp = StackNavigationProp<RootStackParams, 'DetailScreen'>;
+
 export const MoviePoster = ({ movie, width = 300, height = 420 }: Props) => {
   const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  const navigation = useNavigation<DetailscreenNavigationProp>();
 
   return (
-    <View style={{
+    <TouchableOpacity
+    onPress={ () => navigation.navigate('DetailScreen', movie) }
+      activeOpacity={0.8}
+      style={{
         width,
         height,
         marginHorizontal: 8,
@@ -24,7 +32,7 @@ export const MoviePoster = ({ movie, width = 300, height = 420 }: Props) => {
           style={styles.image}
         />
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 

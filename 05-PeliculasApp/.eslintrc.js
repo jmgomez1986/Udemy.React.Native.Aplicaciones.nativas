4019b9ae67c93a6a929efbe593d76a6287558ea9
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-03-21 12:58:23 -0300

    Seccion 11: 178. Obtener los colores de las imágenes

--- 05-PeliculasApp/.eslintrc.js
+++ 05-PeliculasApp/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
         'no-shadow': 'off',
         'no-undef': 'off',
         'prettier/prettier': 'off',
--- 05-PeliculasApp/src/screens/HomeScreen.tsx
+++ 05-PeliculasApp/src/screens/HomeScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ActivityIndicator, View, Dimensions, ScrollView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import ImageColors from 'react-native-image-colors';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
@@ -12,6 +13,15 @@ const { width: windowWidth } = Dimensions.get('window');
 export const HomeScreen = () => {
   const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies();
   const { top: marginTop } = useSafeAreaInsets();
+  const getPosterColours = async (index: number) => {
+    const movie = nowPlaying[index];
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+    const colours = await ImageColors.getColors(uri, {});
+
+    console.log(uri);
+    console.log(colours);
+  };
 
   if (isLoading) {
     return (
@@ -37,6 +47,7 @@ export const HomeScreen = () => {
               sliderWidth={windowWidth}
               itemWidth={300}
               inactiveSlideOpacity={0.9}
+              onSnapToItem={index => getPosterColours(index)}
             />
           </View>
 
@@ -45,7 +56,6 @@ export const HomeScreen = () => {
           <HorizontalSlider title="Proximamente" movies={upcoming} />
         </View>
       </ScrollView>
-
     </GradientBackground>
   );
 };

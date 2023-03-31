db1d1c8d0f4b768ec91aedf72079830d134744ca
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-03-31 16:19:20 -0300

    Seccion 12: 190. Componente - FlatList

--- 06-RNComponents/src/screens/HomeScreen.tsx
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { FlatList } from 'react-native-gesture-handler';
+
+interface MenuItem {
+  name: string;
+  icon: string;
+  components: string;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    components: 'Animations101Screen',
+  },
+];
 
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>{menuItem.name}</Text>
+        <Icon
+          name={menuItem.icon}
+          size={60}
+          color="black" />
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Icon
-        name="star-outline"
-        size={60}
-        color="black" />
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={menuItems}
+        renderItem={({item}) => renderMenuItem(item)}
+        keyExtractor={(item) => item.name}
+      />
     </View>
   );
 };

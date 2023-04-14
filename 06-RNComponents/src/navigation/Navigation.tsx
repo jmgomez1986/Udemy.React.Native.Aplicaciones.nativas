de861de8cad78c470e000ec461b8a2f01608bdc9
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-04-14 17:41:22 -0300

    Seccion 12: 212. Componente - SectionList

--- 06-RNComponents/src/navigation/Navigation.tsx
+++ 06-RNComponents/src/navigation/Navigation.tsx
@@ -7,6 +7,7 @@ import { SwitchScreen } from '../screens/SwitchScreen';
 import { AlertScreen } from '../screens/AlertScreen';
 import { TextInputScreen } from '../screens/TextInputScreen';
 import { PullToRefreshScreen } from '../screens/PullToRefreshScreen';
+import { SectionListScreen } from '../screens/SectionListScreen';
 
 export type RootStackParams = {
   HomeScreen: undefined;
@@ -16,6 +17,7 @@ export type RootStackParams = {
   AlertScreen: undefined;
   TextInputScreen: undefined;
   PullToRefreshScreen: undefined;
+  SectionListScreen: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParams>();
@@ -36,6 +38,7 @@ export const Navigation = () => {
       <Stack.Screen name="AlertScreen" component={AlertScreen} />
       <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
       <Stack.Screen name="PullToRefreshScreen" component={PullToRefreshScreen} />
+      <Stack.Screen name="SectionListScreen" component={SectionListScreen} />
     </Stack.Navigator>
   );
 };

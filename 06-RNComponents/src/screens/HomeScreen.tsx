87dc900d927b22aa152d7af64c37b4b17033710d
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-04-17 14:46:48 -0300

    Seccion 12: 213. Header y Footer de un SectionList

--- 06-RNComponents/src/screens/HomeScreen.tsx
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -5,18 +5,9 @@ import { styles } from '../theme/appTheme';
 import { FlatListMenuItem } from '../components/FlatListMenuItem';
 import { menuItems } from '../data/menuItems';
 import { HeaderTitle } from '../components/HeaderTitle';
+import { ItemSeparator } from '../components/ItemSeparator';
 
 export const HomeScreen = () => {
-  const itemSeparator = () => {
-    return (
-      <View style={{
-        borderBottomWidth: 1,
-        opacity: 0.4,
-        marginVertical: 8,
-      }}/>
-    );
-  };
-
   return (
     <View style={{ flex: 1, ...styles.globalMargin }}>
       <FlatList
@@ -24,7 +15,7 @@ export const HomeScreen = () => {
         renderItem={({item}) => <FlatListMenuItem menuItem={item}/>}
         keyExtractor={(item) => item.name}
         ListHeaderComponent={() => <HeaderTitle title="Opciones de Menu" />}
-        ItemSeparatorComponent={() => itemSeparator()}
+        ItemSeparatorComponent={() => <ItemSeparator />}
       />
     </View>
   );
--- 06-RNComponents/src/screens/SectionListScreen.tsx
+++ 06-RNComponents/src/screens/SectionListScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { SectionList, Text, View } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
+import { ItemSeparator } from '../components/ItemSeparator';
 
 interface Casas {
   casa: string;
@@ -31,12 +32,22 @@ export const SectionListScreen = () => {
         sections={casas}
         keyExtractor={(item, index) => item + index}
         renderItem={({item}) => <Text>{item}</Text>}
+        ListHeaderComponent={() => <HeaderTitle title="Section List" />}
+        ListFooterComponent={() => (
+          <View style={{marginBottom: 70}}>
+            <HeaderTitle title={'Total de casas ' + casas.length} />
+          </View>
+        )}
         stickySectionHeadersEnabled
         renderSectionHeader={({section}) => (
           <View style={{backgroundColor: 'white'}}>
             <HeaderTitle title={section.casa} />
           </View>
         )}
+        renderSectionFooter={({section}) => <HeaderTitle title={'Total: ' + section.data.length} />}
+        SectionSeparatorComponent={() => <ItemSeparator />}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+        showsVerticalScrollIndicator={false}
       />
     </View>
   );

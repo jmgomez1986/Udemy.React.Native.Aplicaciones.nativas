50b3323db35e6c2f81b4310626c36d82f3026949
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-04-20 16:06:08 -0300

    Seccion 13: 226. Snap Carousel

--- 06-RNComponents/src/screens/SlidesScreen.tsx
+++ 06-RNComponents/src/screens/SlidesScreen.tsx
@@ -1,14 +1,93 @@
 import React from 'react';
-import { View } from 'react-native';
-import { styles } from '../theme/appTheme';
+import { Dimensions, Image, ImageSourcePropType, SafeAreaView, Text, View } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 import { HeaderTitle } from '../components/HeaderTitle';
 
+const {height: screenHeight, width: screenWidth} = Dimensions.get('window');
+
+interface Slide {
+  title: string;
+  desc: string;
+  img: ImageSourcePropType;
+}
+
+const items: Slide[] = [
+  {
+    title: 'Titulo 1',
+    desc: 'Ea et eu enim fugiat sunt reprehenderit sunt aute quis tempor ipsum cupidatat et.',
+    img: require('../assets/slide-1.png'),
+  },
+  {
+    title: 'Titulo 2',
+    desc: 'Anim est quis elit proident magna quis cupidatat culpa labore Lorem ea. Exercitation mollit velit in aliquip tempor occaecat dolor minim amet dolor enim cillum excepteur. ',
+    img: require('../assets/slide-2.png'),
+  },
+  {
+    title: 'Titulo 3',
+    desc: 'Ex amet duis amet nulla. Aliquip ea Lorem ea culpa consequat proident. Nulla tempor esse ad tempor sit amet Lorem. Velit ea labore aute pariatur commodo duis veniam enim.',
+    img: require('../assets/slide-3.png'),
+  },
+];
+
 export const SlidesScreen = () => {
+
+  const renderItem = (item: Slide) => {
+    return (
+      <View style={{
+        flex: 1,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        padding: 40,
+        justifyContent: 'center',
+      }}>
+
+        <Image
+          source={item.img}
+          style={{
+            width: 350,
+            height: 400,
+            resizeMode: 'center',
+          }}
+        />
+
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.subTitle}>{item.desc}</Text>
+
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.globalMargin}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        paddingTop: 50,
+      }}>
 
-      <HeaderTitle title="Slides" />
-
-    </View>
+      <Carousel
+        // ref={c => {
+        //   this._carousel = c;
+        // }}
+        data={items}
+        renderItem={({item}) => renderItem(item)}
+        sliderWidth={screenWidth}
+        itemWidth={screenWidth}
+        layout="default"
+      />
+    </SafeAreaView>
   );
 };
+
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#5856D6',
+  },
+  subTitle: {
+    fontSize: 16,
+  },
+});

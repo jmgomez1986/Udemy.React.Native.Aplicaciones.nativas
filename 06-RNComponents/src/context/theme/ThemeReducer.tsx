0a58689aeaa04f3581d94f7b86f18773e8f97b4e
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2023-04-25 14:41:52 -0300

    Seccion 13: 233. ThemeReduce

--- 06-RNComponents/src/context/theme/ThemeReducer.tsx
+++ 06-RNComponents/src/context/theme/ThemeReducer.tsx
@@ -1,5 +1,35 @@
+import { Theme } from '@react-navigation/native';
 import React from 'react';
 
-export const ThemeReducer = () => {
-  return <div>ThemeReducer</div>;
+type ThemeAction =
+  | { type: 'set_light_theme'}
+  | { type: 'set_dark_theme' }
+export interface ThemeState extends Theme {
+  currentTheme: 'light' | 'dark';
+  dividerColor: string;
+}
+
+const lightTheme: ThemeState = {
+  currentTheme: 'light',
+  dividerColor: 'rgba(0,0,0,0.7',
+  dark: false,
+  colors: {
+    primary: 'red',
+    background: 'blue',
+    card: 'green',
+    text: 'pink',
+    border: 'orange',
+    notification: 'teal',
+  }
+}
+
+export const themeReducer = (state: ThemeState, action: ThemeAction): ThemeState => {
+  switch (action.type) {
+    case 'set_light_theme':
+      return {
+        ...lightTheme,
+      };
+    default:
+      return state;
+  }
 };

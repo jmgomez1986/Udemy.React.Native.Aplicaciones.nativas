7a2b27dfc121637c9edf2fbc157e39bd582dd984
Matias <jmgametal@gmail.com>
2023-05-12 16:56:06 -0300

    Seccion 14: 247. Colocar el tipo de dato que retorna la petición HTTP

--- 07-Pokedex/src/hooks/usePokemonPaginated.tsx
+++ 07-Pokedex/src/hooks/usePokemonPaginated.tsx
@@ -1,17 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { pokemonApi } from '../api/pokemonApi';
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
-  const url = 'https://pokeapi.co/api/v2/pokemon?limit=40';
+  const [simplePokemonList, setsimplePokemonList] = useState<SimplePokemon[]>([]);
+  const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
 
   const loadPokemons = async () => {
-    const resp = await pokemonApi.get(url);
-    console.log(resp.data);
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+    nextPageUrl.current = resp.data.next;
+    mapPokemonList(resp.data.results);
+  };
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    pokemonList.forEach(pokemon => console.log(pokemon.name));
   };
 
   useEffect(() => {
     loadPokemons();
   }, []);
 
-  return url;
+  return simplePokemonList;
 };
